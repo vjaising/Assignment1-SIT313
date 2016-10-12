30321d75327a18b4efdfbd7f931b267fd7faeec2
vjaising <vinayjaisinghani@gmail.com>
2016-10-12 23:32:23 +1100

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace CleaningExpert
+{
+	[Activity(Label = "Cleaning Expert", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+
+
+
+		
+		}
+	}
+}
+
+
